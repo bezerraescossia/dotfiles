e20141c9c98c97349c06a7dc9ae94f6f416bf942
Rafael B. Escóssia <bezerraescossia@gmail.com>
2026-08-09 20:12:17 -0300

    style(nvim,neo-tree): define neo-tree position on left

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -3,6 +3,9 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     filesystem = {
+      window = {
+        position = "left",
+      },
       filtered_items = {
         visible = true, -- mostra os itens filtrados (ocultos) por padrão
         hide_dotfiles = false, -- não esconde arquivos que começam com "."
