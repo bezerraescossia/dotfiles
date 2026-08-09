0b1ba9990c29b15b2e2dfea64f2fdca97fca29f8
Rafael B. Escóssia <bezerraescossia@gmail.com>
2026-08-09 16:42:01 -0300

    feat(nvim): add coverage plugin

--- nvim/.config/nvim/lua/plugins/coverage.lua
+++ nvim/.config/nvim/lua/plugins/coverage.lua
@@ -1,23 +1,45 @@
 return {
-  "andythigpen/nvim-coverage",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("coverage").setup {
-      auto_reload = true,
-      commands = true, -- creates :Coverage, :CoverageLoad, :CoverageToggle, etc.
-      highlights = {
-        -- Customize line indicator colors
-        covered = { fg = "#C3E88D" }, -- Soft green for covered lines
-        uncovered = { fg = "#F07178" }, -- Soft red for uncovered lines
-      },
-      signs = {
-        covered = { hl = "CoverageCovered", text = "▎" },
-        uncovered = { hl = "CoverageUncovered", text = "▎" },
-      },
-      summary = {
-        -- Options for the summary window popup
-        min_coverage = 80.0,
-      },
-    }
-  end,
+  {
+    "andythigpen/nvim-coverage",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = {
+      "Coverage",
+      "CoverageLoad",
+      "CoverageShow",
+      "CoverageHide",
+      "CoverageToggle",
+      "CoverageClear",
+      "CoverageSummary",
+    },
+    keys = {
+      { "<leader>tc", "<cmd>CoverageToggle<cr>", desc = "Toggle coverage signs" },
+      { "<leader>tl", "<cmd>CoverageLoad<cr>", desc = "Load coverage report" },
+      { "<leader>ts", "<cmd>CoverageSummary<cr>", desc = "Coverage summary" },
+      { "<leader>th", "<cmd>CoverageHide<cr>", desc = "Hide coverage signs" },
+      { "<leader>tx", "<cmd>CoverageClear<cr>", desc = "Clear coverage data" },
+    },
+    config = function()
+      require("coverage").setup {
+        commands = true, -- create the :Coverage* user commands
+        auto_reload = true, -- automatically reload the report when it changes
+        highlights = {
+          covered = { fg = "#C3E88D" },
+          uncovered = { fg = "#F07178" },
+        },
+        signs = {
+          covered = { hl = "CoverageCovered", text = "▎" },
+          uncovered = { hl = "CoverageUncovered", text = "▎" },
+        },
+        summary = {
+          min_coverage = 80.0, -- threshold used for highlighting in the summary popup
+        },
+        lang = {
+          python = {
+            coverage_file = ".coverage",
+          },
+        },
+      }
+    end,
+  },
 }
+
