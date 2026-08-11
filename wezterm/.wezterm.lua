f36e981b5612c58b65dae1c8fc25b7eeb354ba39
Rafael B. Escóssia <rafael.escossia@eldorado.org.br>
2026-08-11 18:22:49 -0300

    fix(wezterm): remove window decoration

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,7 +6,7 @@ config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 15.0
 config.window_background_opacity = 0.95
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "RESIZE"
+config.window_decorations = "NONE"
 
 config.front_end = "OpenGL"
 config.animation_fps = 60
