e0d1f3e6f4eac7ce90a6bc9af3a23dc6b1be6afe
Rafael B. Escóssia <rafael.escossia@eldorado.org.br>
2026-08-19 08:56:06 -0300

    style(wezterm): change wezterm colorscheme]

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Catppuccin Mocha (Gogh)"
 config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 15.0
 config.window_background_opacity = 0.95
