0036e7c8c0dd2e5884008b3c0c931dad9a6b060e
Rafael B. Escóssia <bezerraescossia@gmail.com>
2026-08-22 17:45:02 -0300

    style(templates): add pyright config to pyproject

--- home/.config/nvim/lua/plugins/astrocore.lua
+++ home/.config/nvim/lua/plugins/astrocore.lua
@@ -73,7 +73,7 @@ return {
 
         -- toggle symbols outline (aerial.nvim)
         ["<Leader>o"] = {
-          function() require("aerial").toggle { direction = "left" } end,
+          function() require("aerial").toggle { direction = "right" } end,
           desc = "Toggle symbols outline",
         },
 
